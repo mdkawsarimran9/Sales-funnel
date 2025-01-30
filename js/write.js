1a31dcf6aa3e91b3395dbc723bfe7c05caf882a6
kawsarimran <162027119+mdkawsarimran9@users.noreply.github.com>
2025-01-30 22:09:08 +0600

    Add files via upload

---
+++ js/write.js
@@ -0,0 +1,67 @@
+AOS.init({
+    offset: 0,
+    delay: 500,
+    duration: 500,
+})
+
+// ************* jQuery ***************
+$(document).ready(function() {
+    $('body').materialScrollTop();
+
+    // slick slider ******
+    $('.test_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        centerPadding: '40px',
+        arrows: true,
+        dots: false,
+        responsive: [
+          {
+            breakpoint: 1399,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 575,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: false
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+})
